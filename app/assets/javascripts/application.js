6c4f2b1676e66b289036c574ba2fb82f8453d70d
Drew Mitchell <jamesandrewmitchell@gmail.com>
2014-11-27 16:39:28 +0000

    adding elevation layer and MIT license

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,12 @@
 $(document).ready(function(){  
   if($('#map').length) {
 		var mapLayer = MQ.mapLayer(); 
+  	  	
+		var contourLayer = L.tileLayer('http://129.206.74.245:8006/tms_il.ashx?x={x}&y={y}&z={z}',{zIndex: 100});
+		var hillshadeLayer = L.tileLayer('http://129.206.74.245:8004/tms_hs.ashx?x={x}&y={y}&z={z}',{zIndex: 100})
+		var roads = L.tileLayer('http://129.206.74.245:8008/tms_rg.ashx?x={x}&y={y}&z={z}',{opacity: .5})			
+		var elevationLayerGroup = L.layerGroup([contourLayer,hillshadeLayer,roads], {maxZoom: 6});
+		
 		var map = L.map('map', {
 	        layers: mapLayer,
 	        center: [ -34.62, -58.381623 ],
@@ -27,10 +33,13 @@ $(document).ready(function(){
 			minZoom: 3
 	    });
   
+
+  
 		L.control.layers({
 	        'Map': mapLayer,
 	        'Satellite': MQ.satelliteLayer(),
-	        'Hybrid': MQ.hybridLayer()
+	        'Hybrid': MQ.hybridLayer(),
+			'Elevation': elevationLayerGroup
 		}).addTo(map); 
 
 		map.on('click', function(e) { 
