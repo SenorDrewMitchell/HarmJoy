31adbede20f5f6cb2db6fd183634d1fdc6deae39
Drew Mitchell <jamesandrewmitchell@gmail.com>
2014-12-05 07:24:37 +0000

    added tag assocition to moments create action

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -23,19 +23,13 @@ class MomentsController < ApplicationController
   def create
     @moment = Moment.new(moment_params)
     @moment.save
+    associate_tags_by_id_array(@moment)
     redirect_to moments_path
   end
 
   def update
     if @moment.update(moment_params)    
-      tags = []
-      if params.has_key?(:comma_seperated_tag_string)
-        tags = Tag.build_tag_id_array_from_comma_seperated_string(params[:comma_seperated_tag_string])
-      end
-      if params[:moment].has_key?(:tag_ids)
-        tags = params[:moment][:tag_ids] + tags
-      end
-      @moment.associate_tags_by_id_array(tags)
+      associate_tags_by_id_array(@moment)
     
       redirect_to moments_path, :alert => "Moment succesfully updated"
     else  
@@ -50,6 +44,17 @@ class MomentsController < ApplicationController
   end
 
   private
+  
+    def associate_tags_by_id_array(moment)
+      tags = []
+      if params.has_key?(:comma_seperated_tag_string)
+        tags = Tag.build_tag_id_array_from_comma_seperated_string(params[:comma_seperated_tag_string])
+      end
+      if params[:moment].has_key?(:tag_ids)
+        tags = params[:moment][:tag_ids] + tags
+      end
+      moment.associate_tags_by_id_array(tags)
+    end
   
     def set_moment
       @moment = Moment.find(params[:id])
